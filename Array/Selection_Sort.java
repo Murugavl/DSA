be67a5def21bbeeefc4b9a8a8713618b41c288f2
vel <vmv2k05@gmail.com>
2025-02-23 19:09:49 +0530

    Selection sort

---
+++ Array/Selection_Sort.java
@@ -0,0 +1,35 @@
+package Array;
+import java.util.Arrays;
+import java.util.Scanner;
+public class Selection_Sort 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of array: ");
+        int n = sc.nextInt();
+        System.out.println("Enter the array elements: ");
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array elements are: " + Arrays.toString(arr));
+        System.out.println("Performing Selection sort...");
+        for(int i = 0; i < n - 1; i++)
+        {
+            int min = i;
+            for(int j = i + 1; j < n; j++)
+            {
+                if (arr[j] < arr[min])
+                {
+                    min = arr[j];
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+        System.out.println("Sorted Array Elements: "+Arrays.toString(arr));
+    }   
+}
